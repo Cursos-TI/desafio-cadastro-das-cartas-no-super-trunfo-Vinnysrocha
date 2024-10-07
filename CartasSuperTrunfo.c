da768a098eb68027e6809fae846eb1f0b0590cdd
Vinnysrocha <165785742+Vinnysrocha@users.noreply.github.com>
2024-10-07 22:18:56 +0000

    super trunfo atualizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -34,7 +34,7 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-    printf("\n<<INFORMAÇÕES DAS CARTAS>>\n");
+    printf("\n<<INFORMAÇÕES DA CARTA>>\n");
     printf("Codigo: %d\n",codigo);
     printf("Nome: %s\n",nome);
     printf("População: %d\n",populacao);
